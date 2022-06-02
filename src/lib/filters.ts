2170e31fb4217f837e1d1874215277a10cd6eee3
wangjian27 <1223028562@qq.com>
2022-06-02 16:55:17 +0800

    完成

--- src/lib/filters.ts
+++ src/lib/filters.ts
@@ -1,6 +1,6 @@
 /*---------------------注册全局过滤器----------------------*/
 //无数据情况处理，传入空字符串
-const judgeIsNull = function (val: any) {
+const judgeIsNull = function (val: any): string {
   const target = typeof val === 'object' ? Object.prototype.toString.call(val) : `${val}`;
   if (/null|undefined|nan|^\s*$/i.test(target)) {
     return '--';
@@ -9,13 +9,13 @@ const judgeIsNull = function (val: any) {
 };
 
 //保留两位小数处理
-const decimal = function (val: string) {
+const decimal = function (val: string): string {
   const two = 2;
   return judgeIsNull(val) === '--' ? '--' : parseFloat(val).toFixed(two);
 };
 
 //针对正数加上+
-const positive = function (val: string) {
+const positive = function (val: string): string {
   if (judgeIsNull(val) === '--') {
     return '--';
   } else if (parseFloat(val) > 0) {
@@ -26,17 +26,17 @@ const positive = function (val: string) {
 };
 
 //针对有值的带上百分号
-const addPercentSymbol = function (val: any) {
+const addPercentSymbol = function (val: any): string {
   return judgeIsNull(val) === '--' ? '--' : `${val}%`;
 };
 
 //去除股票名称的空格
-const removeSpace = function (val: string) {
+const removeSpace = function (val: string): string {
   return judgeIsNull(val) === '--' ? val.replace(/\s/g, '') : '--';
 };
 
 //时间戳转换为日期
-const stampToDate = function (val: string | number) {
+const stampToDate = function (val: string | number): string {
   return Boolean(+val) ? new Date(+val).toTimeString().split(' ')[0] : '--';
 };
 
